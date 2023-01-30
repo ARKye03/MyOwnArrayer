1ce360d3983874fd699717e7e26c37b4a7833b4e
ARKye03 <121306859+ARKye03@users.noreply.github.com>
2023-01-29 19:33:31 -0500

    Add files via upload

---
+++ C-DynArray/Arrayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Darr
+{ 
+    internal class Mind
+    {
+        public static void AMain()
+        {
+            //Console.WriteLine("Cantidad de items");
+
+            FileStream ost;
+            StreamWriter sw;
+            TextWriter Oo = Console.Out;
+
+            try
+            {
+                ost = new FileStream ("./Arrayed.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                sw = new StreamWriter (ost);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine ("Cannot open Arrayed.txt for writing");
+                Console.WriteLine (e.Message);
+                return;
+            }
+             Console.SetOut(sw);     
+             Console.Write(Program.Arrayer() + ";");
+             Console.SetOut(Oo);
+             sw.Close();
+             ost.Close();
+             Console.WriteLine("Done");
+            
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ C-DynArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Darr;
+
+internal class Program
+{
+    static string[] farray;
+    static void Main()
+    {
+        int d = 0;
+        bool sigue = true;
+        Console.WriteLine("Introduce elementos------------------Type " + '"' + "stop" + '"' + "to parar obviamente");
+        List<string> Dyn = new List<string>();
+        do
+        {
+            Dyn.Add(Console.ReadLine());
+            int y = Dyn.Count;
+            if (Dyn[d] == "stop")
+            sigue = false;
+
+            d++;
+            
+        } while (sigue);
+
+        Dyn.Remove("stop");
+
+        //for (int i = 0; i < Dyn.Count; i++){ Console.WriteLine(Dyn[i]);}
+
+        farray = Dyn.ToArray();
+        Console.Clear();
+
+        for (int k = 0; k < farray.Length; k++)
+        {
+            Console.WriteLine(farray[k]);
+        } 
+        Mind.AMain();
+        Console.ReadKey();
+    }
+    public static string[] Arrayer(){
+            Console.Clear();
+
+            for (int d = 0; d < farray.Length; d++){ Console.Write('"' + farray[d] + '"' + ',');}
+
+            return farray;
+        }
+}
